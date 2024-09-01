f61ceea1bc143fcd1b66515aa93af0ef7292b875
Rifat <tmrifat87@gmail.com>
2024-09-01 19:05:33 +0600

    Final

--- my_first_react_app/src/App.js
+++ my_first_react_app/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import MovieCard from './MovieCard';    
 // c90ac550 omdbapi key value
@@ -16,11 +16,14 @@ const movie1 = {
 }
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
+
     const searchMovies = async (title) => {
         const res = await fetch(`${API_URL}&s=${title}`);
         const data = await res.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -34,19 +37,30 @@ const App = () => {
             <div className='search'>
                 <input 
                     placeholder='Search for movies'
-                    value="Superman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
-            <div className='container'>
-                <MovieCard movie1={movie1} />
-            </div>
+            {
+                movies?.length > 0
+                ?(
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
         </div>
     );
 }
--- my_first_react_app/src/MovieCard.jsx
+++ my_first_react_app/src/MovieCard.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 
 //  Now we are going to  use props for  multiple use,  but  we can't  use props here, otherwise we have to add props to everywhere. Thats why we will use destructuring
-const MovieCard = ({ movie1 }) => {
+const MovieCard = ({ movie }) => {
     return(
         <div className="movie">
             <div>
-                <p>{movie1.Year}</p>
+                <p>{movie.Year}</p>
             </div>
             <div>
-                <img src={movie1.Poster !== 'N/A'?movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title} />
+                <img src={movie.Poster !== 'N/A'?movie.Poster: 'https://via.placeholder.com/400'} alt={movie.Title} />
             </div>
             <div>
-                <span>{movie1.Type}</span>
-                <h3>{movie1.Title}</h3>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
             </div>
         </div>
     )
